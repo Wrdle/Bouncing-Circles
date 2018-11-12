747ee8dab3daed129d74e2762f7691f258714eb4
Matt <mdagostino175@gmail.com>
2018-11-12 14:54:52 +1000

    Uploaded Code

---
+++ Circles/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Circles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.canvas = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(800, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Bouncing!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // canvas
+            // 
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(800, 402);
+            this.canvas.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Bouncing";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel canvas;
+    }
+}
+
---
+++ Circles/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Circles
+{
+    public partial class Form1 : Form
+    {
+        public List<Circle> circles = new List<Circle>();
+        public Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread drawCircles = new Thread(new System.Threading.ThreadStart(animateCircles));
+            drawCircles.Start();
+        }
+
+        public void generateCircles()
+        {
+            circles.Clear();
+            Random rand = new Random();
+            for (int x = 0; x < 1; x++)
+            {
+                Circle circle = new Circle(rand.Next(1, 300), rand.Next(1, 300), 1, 1);
+                circles.Add(circle);
+            }
+        }
+
+        public void animateCircles()
+        {
+            g = canvas.CreateGraphics();
+            generateCircles();
+            Brush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+
+            for (int iterations = 0; iterations < 1500; iterations++)
+            {
+                Random rand = new Random();
+                for (int x = 0; x < 1; x++)
+                {
+                    int newXPos = circles[x].xDir;
+                    int newYPos = circles[x].yDir;
+                    if (circles[x].yPos <= 0)
+                    {
+                        newYPos = 1;
+                    }
+                    if (circles[x].yPos >= canvas.Height)
+                    {
+                        newYPos = -1;
+                    }
+                    if (circles[x].xPos <= 0)
+                    {
+                        newXPos = 1;
+                    }
+                    if (circles[x].xPos >= canvas.Width)
+                    {
+                        newXPos = -1;
+                    }
+                    Circle circle = new Circle(circles[x].xPos + newXPos, circles[x].yPos + newYPos, newXPos, newYPos);
+                    circles[x] = circle;
+                    g.FillCircle(myBrush, circles[x].xPos, circles[x].yPos, 5);
+                }
+                Thread.Sleep(5);
+                g.Clear(Color.White);
+            }
+        }
+    }
+
+    public struct Circle
+    {
+        public int xPos;
+        public int yPos;
+        public int xDir;
+        public int yDir;
+
+       public Circle(int _xPos, int _yPos, int _xDir, int _yDir)
+        {
+            xPos = _xPos;
+            yPos = _yPos;
+            xDir = _xDir;
+            yDir = _yDir;
+        }
+    }
+
+
+    public static class GraphicsExtensions
+    {
+        public static void DrawCircle(this Graphics g, Pen pen,
+                                      float centerX, float centerY, float radius)
+        {
+            g.DrawEllipse(pen, centerX - radius, centerY - radius,
+                          radius + radius, radius + radius);
+        }
+
+        public static void FillCircle(this Graphics g, Brush brush,
+                                      float centerX, float centerY, float radius)
+        {
+                g.FillEllipse(brush, centerX - radius, centerY - radius, radius + radius, radius + radius);
+        }
+        public static void clearScreen(this Graphics g)
+        {
+            g.Clear(Color.White);
+        }
+    }
+}
